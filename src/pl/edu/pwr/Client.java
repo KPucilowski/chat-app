476256fe4d62e5307c8bdf15624deb929e228ebf
AdamLekan <lenka909@onet.eu>
2021-11-09 20:45:54 +0100

    server i clinet

---
+++ src/pl/edu/pwr/Client.java
@@ -0,0 +1,75 @@
+package pl.edu.pwr;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.*;
+import java.util.Base64;
+import java.util.Scanner;
+public class Client {
+    private static String key="";
+
+    public static void main(String[] args){
+        final Socket clientSocket;
+        final BufferedReader in;
+        final PrintWriter out;
+        final Scanner sc = new Scanner(System.in);
+
+        try {
+            clientSocket = new Socket("localhost", 5000);
+            out = new PrintWriter(clientSocket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            Thread sender = new Thread(new Runnable() {
+                String msg ;
+
+                @Override
+                public void run() {
+                    out.println(RSAutils.publicKey);
+                    out.flush();
+                    msg = sc.nextLine();
+                    while(msg!=null){
+                        msg = sc.nextLine();
+                        out.println(msg);
+//                        try {
+//                       //     out.println(Base64.getEncoder().encodeToString(RSAutils.encrypt(msg, key)));
+//                        } catch (NoSuchPaddingException | NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | InvalidKeyException e) {
+//                            e.printStackTrace();
+//                        }
+                        out.flush();
+                    }
+                }
+            });
+            sender.start();
+
+            Thread receiver = new Thread(new Runnable() {
+                String msg;
+                @Override
+                public void run() {
+
+                    try {
+                        key = in.readLine();
+                        msg = in.readLine();
+                        while(msg!=null){
+                            System.out.println("Server : " + RSAutils.decrypt(msg, RSAutils.privateKey));
+                            msg = in.readLine();
+                        }
+                        System.out.println("Połączenie przerwane");
+                        out.close();
+                        clientSocket.close();
+                    } catch (IOException | IllegalBlockSizeException | NoSuchPaddingException | BadPaddingException | NoSuchAlgorithmException | InvalidKeyException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            receiver.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/pl/edu/pwr/Server.java
@@ -0,0 +1,71 @@
+package
+        pl.edu.pwr;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.*;
+import java.util.Base64;
+import java.util.Scanner;
+
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        final ServerSocket serverSocket ;
+        Socket clientSocket ;
+        int id = 1;
+        serverSocket = new ServerSocket(5050);
+        while (true)
+        {
+            clientSocket = serverSocket.accept();
+            ClientThread clientThread = new ClientThread(clientSocket, id++);
+            clientThread.start();
+        }
+    }
+}
+class ClientThread extends Thread {
+
+    Socket clientSocket;
+    int clientID ;
+    final Scanner sc = new Scanner(System.in);
+
+    ClientThread(Socket s, int i)
+    {
+        clientSocket = s;
+        clientID = i;
+    }
+    public void run() {
+        System.out.println("Client ID - " + clientID + " : Address - " + clientSocket.getInetAddress().getHostName());
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+//            out.println(RSAutils.publicKey);
+//            out.flush();
+
+            String key = in.readLine();
+            //  System.out.println(" klucz clienta " +clientID + " " +  key );
+
+            while (true) {
+                String clientCommand = in.readLine();
+                System.out.println("Client " + clientID +" : "+ RSAutils.decrypt(clientCommand, RSAutils.privateKey));
+                out.flush();
+
+                if (clientCommand.equals("q")) {
+                    System.out.print("Client " + clientID + " rozłączył się");
+                }
+                else {
+                    String command = sc.nextLine();
+                    out.println(Base64.getEncoder().encodeToString(RSAutils.encrypt(command, key)));
+                    out.flush();
+                }
+            }
+        } catch (IOException | InvalidKeyException | NoSuchAlgorithmException | BadPaddingException | NoSuchPaddingException | IllegalBlockSizeException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+}
